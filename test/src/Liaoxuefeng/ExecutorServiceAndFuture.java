04c5374fe04cf28bb23844dd007e14ed4c571823
e <tianyuhuan@haier.com>
2022-07-29 16:22:28 +0800

    Daily Practice For Java Base

--- test/src/Liaoxuefeng/ExecutorServiceAndFuture.java
+++ test/src/Liaoxuefeng/ExecutorServiceAndFuture.java
@@ -1,7 +1,29 @@
 package Liaoxuefeng;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.*;
+
 public class ExecutorServiceAndFuture {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(4);
+        Future<BigDecimal> future = es.submit(new Task("601857"));
+        System.out.println(future.get());
+        es.shutdown();
+    }
+}
+
+
+class Task implements Callable<BigDecimal> {
+
+    public Task(String code) {
+
+    }
+
+    @Override
+    public BigDecimal call() throws Exception {
+        Thread.sleep(1000);
+        double d = 5 + Math.random() * 20;
+        return new BigDecimal(d).setScale(2, RoundingMode.DOWN);
     }
 }

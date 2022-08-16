7f68ddef7b5329056c0932379554a48e1c80fccd
tianyuhuan <tianyuhuan@haier.com>
2022-08-16 17:44:03 +0800

    Daily Practice

--- test/src/main/java/Liaoxuefeng/test.java
+++ test/src/main/java/Liaoxuefeng/test.java
@@ -1,16 +1,49 @@
 package Liaoxuefeng;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.File;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-//        File f = new File(test.class.getResource("/insertData.txt").getFile());
-        File f = new File(test.class.getResource("/insertData.txt").getFile());
-        System.out.println(f.exists());
-        System.out.println(Arrays.toString("this is a test".split(" ")));
+
+        System.out.println("9991ds".matches("9{4}.*"));
+        Map<String,String> map = new HashMap<>();
+        map.put("9991ds","111");
+        System.out.println(map.get("9991ds"));
+        String s = "5";
+        s.replaceAll("%/%","25");
+        System.out.println(s);
+        MyData m = new MyData("Bob","english",54);
+        System.out.println(JSONObject.toJSONString(m));
+        String js = "{\"course\":\"english\",\"name\":\"Alice\",\"score\":3}";
+        JSONObject jo = new JSONObject();
+        jo.put("course","english");
+        jo.put("score",34);
+        jo.put("score",56);
+        jo.put("name","Mark");
+        jo.remove("name");
+        System.out.println(jo.keySet());
+        MyData m2 = JSONObject.parseObject(js,MyData.class);
+        System.out.println(JSONObject.toJSONString(m2));
+
+        System.out.println(JSONObject.toJSONString("sdsa"));
+
+
     }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class MyData{
+    private String name;
+    private String course;
+    private int score;
 }
